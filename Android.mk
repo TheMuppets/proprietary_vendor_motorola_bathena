e4c6c928f4a045db318b4529ec3e53b1e344bc9d
Michael Bestas <mkbestas@gmail.com>
2025-07-19 18:40:23 +0300

    bathena: Import blobs & firmware from RZD31.31

---
+++ Android.mk
@@ -0,0 +1,27 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),bathena)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,4a1352992db76480001fd037d766d6a059ea1d73)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,404ac64f12c04ffb7db41856c3aac09859df15ee)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,ee5be83062e483d07aa670bb73e84bd1b6db00d9)
+$(call add-radio-file-sha1-checked,radio/dsp.img,a4d96325a5d65cc91374e29ba29d7bbf19f50bd7)
+$(call add-radio-file-sha1-checked,radio/fsg.img,b782b4e9d8a3a0572b6747bee454dff822023d89)
+$(call add-radio-file-sha1-checked,radio/hyp.img,31e579ac7e96fa8fa8cfa4ca18e58aab34bf78b3)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,51649069a89910cc18aa933358f455ca4ea15a68)
+$(call add-radio-file-sha1-checked,radio/logo.img,7b4846d4305f888bc3b7dc7dac4574641fccc585)
+$(call add-radio-file-sha1-checked,radio/modem.img,c7bae78e22428a9d511abbdf55129ef63002bf5f)
+$(call add-radio-file-sha1-checked,radio/prov.img,ef5275c0440f0ee4536d5ae5c5918c09e7dff5ed)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,17a650917d4d377ccf7e987331c171aac8213654)
+$(call add-radio-file-sha1-checked,radio/rpm.img,cd660b1011370fd5b55468775242fe73a43c9cb7)
+$(call add-radio-file-sha1-checked,radio/storsec.img,c09d933eb83e4913737f8889843ec3325865692f)
+$(call add-radio-file-sha1-checked,radio/tz.img,f4a52b192dccff81d459674e11124e77f1ff7463)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,c4b477ff5f3a603086b30c4a8120746878d8ecfb)
+$(call add-radio-file-sha1-checked,radio/xbl.img,68c37b00b68a9c854229e6957e64ae12cb4e0ad8)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,3a9030355440457b8b4dc520621f930713786845)
+
+endif
